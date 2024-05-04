feb70b18dfef57eb6c3f15de3484d8106ce9f3d7
Riya Patel <rpatel05@terpmail.umd.edu>
2024-05-04 17:32:38 -0400

    Added re-roll options and tuple message

--- consolidation_project_2.py
+++ consolidation_project_2.py
@@ -28,3 +28,16 @@ def main():
             turn = 0
             dice = rolling_dice()
             print("You rolled: ", dice)
+
+            if len(set(dice)) == 1:
+                print("This is a tuple! You receive 0 points.")
+            else:
+                fixed_dice = set()
+                while True:
+                    choose = input("Do you want to re-roll any dice? (yes/no): ").lower()
+                    if choose != 'yes':
+                        break
+                    else:
+                        fixing_dice = input("Enter the dice you want to fix (1, 2, or 3), or input '0' to stop: ")
+                        if fixing_dice == '0':
+                            break
