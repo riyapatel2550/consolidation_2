f5aadd0dec23f4e361ddd3a687e68b3a623bafaf
Riya Patel <rpatel05@terpmail.umd.edu>
2024-05-04 16:01:47 -0400

    Started code and created def functions for rolling and score

--- consolidation_project_2.py
+++ consolidation_project_2.py
@@ -1,3 +1,20 @@
 # "Tuple Out" Dice Game (Code)
+# Install dependenes if needed (conda install)
 
-print("Hello World")
+import os
+import random
+
+def rolling_dice():
+    return [random.randint(1, 6) for _ in range(3)]
+
+def score(dice):
+    if len(set(dice)) == 1:
+        return 0
+    else:
+        return sum(dice)
+    
+def main():
+    print("Welcome to the 'Tuple Out' Dice Game!")
+    while True:
+        input("Press enter for rolling the dice.")
+        dice = rolling_dice()
