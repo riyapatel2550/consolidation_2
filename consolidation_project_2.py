3cc4989408ead2436b670dad6ab6b32f8567835d
Riya Patel <rpatel05@terpmail.umd.edu>
2024-05-04 17:21:42 -0400

    Started main method and introduce rules/players with while and for loop

--- consolidation_project_2.py
+++ consolidation_project_2.py
@@ -1,20 +1,30 @@
 # "Tuple Out" Dice Game (Code)
 # Install dependenes if needed (conda install)
 
-import os
 import random
+import os
 
 def rolling_dice():
+    """Roll three dice and return the result."""
     return [random.randint(1, 6) for _ in range(3)]
 
 def score(dice):
-    if len(set(dice)) == 1:
+    """Calculate the score based on the rolled dice."""
+    if len(set(dice)) == 1:  
         return 0
     else:
         return sum(dice)
-    
+
 def main():
-    print("Welcome to the 'Tuple Out' Dice Game!")
-    while True:
-        input("Press enter for rolling the dice.")
-        dice = rolling_dice()
+    print("Welcome to the 'Tuple Out' Dice Game! There will be two rounds!")
+    print("You lose if you have three of the same number.")
+    print("Do not re-roll any dice that are of the same number.")
+    players_number = int((input("Enter number of players: ")))
+    scores = [0] * players_number
+    rounds = 0
+    while rounds < 2:
+        for player in range(players_number):
+            print(f"Player {player + 1}'s turn:")
+            turn = 0
+            dice = rolling_dice()
+            print("You rolled: ", dice)
